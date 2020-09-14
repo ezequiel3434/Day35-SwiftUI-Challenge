5f8c87246703b66ce715c16a1b4deaa0a8493f50
ezequiel3434 <ezequiel3434@gmail.com>
2020-09-14 17:36:08 -0300

    refactoring and Question Model

---
+++ Day35-SwiftUI-Challenge/Model/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Day35-SwiftUI-Challenge
+//
+//  Created by Ezequiel Parada Beltran on 14/09/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import Foundation
+
+struct Question: Hashable {
+    let firstNumber:Int
+    let secondNumber: Int
+    var tableNumber: Int = 0
+    
+    var goodAnswer: Int {
+        return firstNumber * secondNumber
+    }
+    var position: Int
+    var wellAnswer = false
+}
--- Day35-SwiftUI-Challenge/View/ContentView.swift
+++ Day35-SwiftUI-Challenge/View/ContentView.swift
@@ -9,9 +9,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+
     var body: some View {
-        Text("Hello, World!")
+        Text("hello")
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
